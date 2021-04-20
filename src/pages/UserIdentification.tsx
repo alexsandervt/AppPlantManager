2f2867ec8880454023458d9c9c1ddeb2b2117974
Alexsander Telles <git@alextelles.dev.br>
2021-04-20 11:51:19 -0300

    Navegação e Páginas

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Platform,
+  Keyboard,
+} from "react-native";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+import { Button } from "../components/Button";
+import { useNavigation } from "@react-navigation/core";
+
+export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFiled, setIsfiled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  const navigation = useNavigation();
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsfiled(!!name);
+  }
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+  function handleInputChange(value: string) {
+    setIsfiled(!!value);
+    setName(value);
+  }
+  function handleSubmite() {
+    navigation.navigate("Confirmation");
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.content}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <Text style={styles.emoji}>{isFiled ? "😄" : "😝"}</Text>
+              <Text style={styles.title}>
+                Como podemos {"\n"}
+                chamar você?
+              </Text>
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFiled) && { borderColor: colors.green },
+                ]}
+                placeholder="Digite seu nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+              <View style={styles.footer}>
+                <Button onPress={handleSubmite} title={"Confirmar"} />
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: " 100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 54,
+    alignItems: "center",
+  },
+  emoji: {
+    fontSize: 44,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: "100%",
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: "center",
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 20,
+  },
+  footer: {
+    marginTop: 40,
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+});
